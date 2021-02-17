0c40c7b09c0bef290bd00be94155d2f38a2b2a00
Vince Garcia <garciaarr@gmail.com>
2021-02-16 19:30:54 -0700

    Revert "Merge branch 'master' into vince"
    This reverts commit b3c9bb0fdbac4cdce5d39ddd48b6d7b256f2fd92, reversing
    changes made to a8e7fcd5b149720a60c8bd030340a6f8d9e20bd6.

--- Lesson-59-behind-scenes-01-starting-setup/behind-scenes-01-starting-setup/app.js
+++ Lesson-59-behind-scenes-01-starting-setup/behind-scenes-01-starting-setup/app.js
@@ -10,9 +10,61 @@ const app = Vue.createApp({
       this.currentUserInput = event.target.value;
     },
     setText() {
-      this.message = this.currentUserInput;
+      // this.message = this.currentUserInput;
+      console.log(this.$refs);
+      this.message = this.$refs.userText.value;
     },
+  },
+  beforeCreate() {
+    console.log('beforeCreate()');
+  },
+  created() {
+    console.log('created()');
+  },
+  beforeMount() {
+    console.log('beforeMounted()');
+  },
+  mounted() {
+    console.log('mounted()');
+  },
+  beforeUpdate() {
+    console.log('beforeUpdate()')
+  },
+  updated() {
+    console.log('updated()')
   },
 });
 
 app.mount('#app');
+
+// ....
+
+// let message = 'Hello';
+// let longMessage = message + ' World!';
+
+// console.log(longMessage);
+
+// message = 'Hello!!!!!';
+
+// console.log(longMessage);
+
+const data = {
+  message: 'hello',
+  longMessage: ''
+};
+
+const handler1 = {
+  set(target, key, value) {
+    if (key === 'message') {
+      target.longMessage = value + ' World!'
+    }
+    target.message = value;
+  }
+};
+
+const proxy1 = new Proxy(data, handler1);
+
+proxy1.message = "Hello!!!!";
+
+console.log('This is the data object', data);
+console.log('This is the proxy object', proxy1)
