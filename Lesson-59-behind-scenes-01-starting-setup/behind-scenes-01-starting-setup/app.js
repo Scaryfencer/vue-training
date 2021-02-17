e29f24711d5b91dcbfe60c76f3c221fa298f8737
Don Gordon <OpNop@users.noreply.github.com>
2021-02-16 19:07:22 -0700

    why does Git suck sometimes

--- Lesson-59-behind-scenes-01-starting-setup/behind-scenes-01-starting-setup/app.js
+++ Lesson-59-behind-scenes-01-starting-setup/behind-scenes-01-starting-setup/app.js
@@ -10,9 +10,58 @@ const app = Vue.createApp({
       this.currentUserInput = event.target.value;
     },
     setText() {
-      this.message = this.currentUserInput;
+      this.message = this.$refs.userText.value
     },
+  },
+  beforeCreate() {
+    console.log('beforeCreate');
+  },
+  created() {
+    console.log('created');
+  },
+  beforeMount() {
+    console.log('beforeMount');
+  },
+  mounted() {
+    console.log('mounted');
+  },
+  beforeUpdate() {
+    console.log('beforeUpdate');
+  },
+  updated() {
+    console.log('updated');
+  },
+  beforeUnmount() {
+    console.log('beforeUnmounted')
+  },
+  unmounted() {
+    console.log('unmounted')
   },
 });
 
 app.mount('#app');
+
+// ....
+// let message = "Hello!";
+// let longMessage = message + ' World!';
+// console.log(longMessage);
+// message = 'Hello!!!!';
+// console.log(longMessage);
+
+// const data = {
+//   message: 'Hello!',
+//   longMessage: 'Hello! World!'
+// };
+
+// const handle = {
+//   set(target, key, value) {
+//     if(key === 'message'){
+//       target.longMessage = value + ' World'
+//     }
+//     target.message = value;
+//   }
+// };
+// const proxy = new Proxy(data, handle);
+
+// proxy.message = 'Hello!!!!';
+// console.log(proxy.longMessage);
